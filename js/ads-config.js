174e1d5e06656ec2c2bceb656f723bd310191f69
Ledesmita <160266656+LedesmitaDJ@users.noreply.github.com>
2026-02-01 14:02:39 -0500

    Add files via upload

--- js/ads-config.js
+++ js/ads-config.js
@@ -2,14 +2,14 @@ window.ADS_CONFIG = {
   ledesmita: [
     {
       type: "video",
-      src: "../vid/LR-AD01.mp4",
+      src: "https://ledesmitadj.github.io/LedesmitaTV/vid/LR-AD01.mp4",
       skipAfter: 5,
       link: "https://play.google.com/store/apps/details?id=ledesmita.radio",
       cta: "Descargar App"
     },
     {
       type: "image",
-      src: "../img/SM-LTV-AD01.gif",
+      src: "https://ledesmitadj.github.io/LedesmitaTV/img/SM-LTV-AD01.gif",
       duration: 5,
       skipAfter: 5,
       link: "https://www.tiktok.com/@ledesmita.tv",
